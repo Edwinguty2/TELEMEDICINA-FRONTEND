4523d5a673f19075b1e7bf23cc5bc30cc7a3bd94
Samuel Espitia <samuelespitia88@gmail.com>
2025-03-28 22:35:05 -0500

    Agrega lÃimites y fechas en punto

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import InputField from './InputField';
 import SubmitButton from './SubmitButton';
@@ -6,25 +6,78 @@ import SubmitButton from './SubmitButton';
 const AppointmentForm = ({ onSubmit }) => {
   const { register, handleSubmit } = useForm();
 
-  const currentDateTime = new Date().toISOString().slice(0, 16);
+  const [selectedHour, setSelectedHour] = useState('');
+
+  const currentDate = new Date().toISOString().slice(0, 10);
+
+  const maxDate = new Date();
+  maxDate.setDate(maxDate.getDate() + 7);
+  const formattedMaxDate = maxDate.toISOString().slice(0, 10);
+
+  const availableHours = [];
+  for (let hour = 8; hour <= 17; hour++) {
+    const isAM = hour < 12;
+    const hourIn12 = hour % 12 === 0 ? 12 : hour % 12;
+    const period = isAM ? 'AM' : 'PM';
+    const time = `${hourIn12}:00 ${period}`;
+    availableHours.push(time);
+  }
+
+  const cedulaPaciente = localStorage.getItem('cedula');
+
+  const convertTo24HourFormat = (hour) => {
+    const [hourPart, period] = hour.split(' ');
+    let [hh, mm] = hourPart.split(':');
+
+    if (period === 'PM' && hh !== '12') {
+      hh = (parseInt(hh) + 12).toString();
+    } else if (period === 'AM' && hh === '12') {
+      hh = '00';
+    }
+
+    hh = hh.padStart(2, '0');
+
+    return `${hh}:${mm}:00`;
+  };
+
+  const handleFormSubmit = (data) => {
+    const selectedDate = data.fecha;
+    const formattedHour = convertTo24HourFormat(selectedHour);
+
+    const formattedDateTime = `${selectedDate}T${formattedHour}`;
+
+    const formData = { ...data, cedulaPaciente, fechaHora: formattedDateTime };
+    onSubmit(formData);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
       <InputField
-        label="Cédula del Paciente"
-        type="text"
-        name="cedulaPaciente"
+        label="Fecha"
+        type="date"
+        name="fecha"
         register={register}
         required
+        min={currentDate}
+        max={formattedMaxDate}
       />
-      <InputField
-        label="Fecha y Hora"
-        type="datetime-local"
-        name="fechaHora"
-        register={register}
-        required
-        min={currentDateTime} // Establece la fecha y hora mínima
-      />
+
+      <div>
+        <label>Hora</label>
+        <select
+          name="hora"
+          {...register("hora")}
+          onChange={(e) => setSelectedHour(e.target.value)}
+          value={selectedHour}
+          required
+        >
+          <option value="">Selecciona una hora</option>
+          {availableHours.map((hour, index) => (
+            <option key={index} value={hour}>{hour}</option>
+          ))}
+        </select>
+      </div>
+
       <SubmitButton label="Agendar" />
     </form>
   );
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const InputField = ({ label, type, name, register, required }) => {
+const InputField = ({ label, type, name, register, required, min, max}) => {
   return (
     <div>
       <label>{label}</label>
-      <input type={type} {...register(name)} required={required} />
+      <input
+        type={type}
+        name={name}
+        {...register(name)}
+        required={required}
+        min={min}
+        max={max}
+      />
     </div>
   );
 };
